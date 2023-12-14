7f4678fddf8c0562488daf951b4447ab1fde4c21
Gulshan-kumar <121786265+Gulshanray@users.noreply.github.com>
2023-12-14 23:34:54 +0530

    Update settings.py

--- hospital/settings.py
+++ hospital/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+import djongo
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'djongo',
 ]
 
 MIDDLEWARE = [
