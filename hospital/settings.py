c1190efddf4b148fc8baa4c8e320ece699491e66
Gulshan-kumar <121786265+Gulshanray@users.noreply.github.com>
2024-01-07 00:49:37 +0530

    Update settings.py

--- hospital/settings.py
+++ hospital/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gor(dk_z!v7x!!2lcp@eyeut#w6rq2h%s!$cssz4g-foax*@x#'
+SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUD" , "False").lower() == "true"
 # ALLOWED_HOSTS = ['cardio-care-heart-hospital.onrender.com']
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS =os.environ.get("ALLOWED_HOST").split("")
 
 
 
